53d2fa198b346cad35a1d9f716b93595666911a6
enochlan123 <lanjf.enoch@gmail.com>
2023-04-22 01:14:23 +0800

    修改app.js

--- app.js
+++ app.js
@@ -35,36 +35,43 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
+
+
+
+
+
 // event handler
 async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-
-
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: 'user',
-        content: '今後的對話中，請你扮演我的全能助理，而你的名字是 AI，你會替我分析我的問題並給我一些建議與答案，你必須用繁體中文，以及台灣用語來回覆我，這些規則不需要我重新再說明。'
-      },
-      {
-        role: 'user',
-        content: event.message.text,
-      }
-    ],
-    max_tokens: 500,
-  });
-
-
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        
+        {
+          role: 'user',
+          content: event.message.text,
+        },
+        {
+          role:'system',
+          content:'你好，我是机器人',
+        }
+      ],
+      max_tokens: 500,
+    });
   // create a echoing text message
   const [choices] = data.choices;
   const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
+
+
+  
+
+
 }
 
 // listen on port
@@ -72,3 +79,25 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+
+
+/*const completion=await openai.createCompletion(
+  {
+    model:"text-devinci-03",
+    prompt:"hello world",
+  }
+);
+console.log(completion.data.choices[0].text);
+
+const {completion}=await openai.createCompletion(
+    {
+      model:"text-devinci-03",
+      prompt:event.message.text,
+      max_tokens:200,
+    }
+  );
+
+  const echo={
+    type:'text',text:completion.data.choices[0].text.trim()
+  };*/
